9319f559723c631b09a0efdb9be986a803b7a36b
Artur Ferreira <artur.ferreira@ckw.ch>
2025-06-16 20:49:10 +0000

    added more empty models, migration and controller for comment and topic

--- api/app/Models/Post.php
+++ api/app/Models/Post.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     //
+    protected $fillable = ['title', 'content', 'author_id'];
+    protected $hidden = ['created_at', 'updated_at'];
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'author_id');
+    }
 }
